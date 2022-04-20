cf095bdf8364bd4a5538ef936ab40593ff2a5043
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 18:44:06 +0000

    build based on f76111d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Dedisp","category":"page"},{"location":"#Dedisp","page":"Home","title":"Dedisp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Dedisp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Dedisp]","category":"page"}]
+}
